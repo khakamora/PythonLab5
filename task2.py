fcff32ef65e16178c7477804fa7c0fc84866fd75
khakanora <yuriy.mamchurovskyy@gmail.com>
2018-11-01 13:41:14 +0200

    working ...

--- task2.py
+++ task2.py
@@ -1,52 +1,30 @@
-'''
-Created on 28 жовт. 2018 р.
+import csv
 
-@author: F
-'''
+with open('students.csv', 'r', encoding='utf-16') as myfiles:
+    reader = csv.reader(myfiles, delimiter=';')
+    student_list = [row for row in reader]
+#print(student_list)
+student_list.pop(0)
 
-with open("students.csv", "r", encoding='utf-8') as ins:
-    array = []
-    for line in ins:
-        array.append([line])
+n = input('Сортировать по номеру группы (4), фамилии (2), возрасту (3): ')
+n = int(n)-1
 
-#with open('students.csv', 'r', encoding='utf-8') as f: lines = [line for line in f]
+print(n)
 
-#test_lines = [x.rstrip('\n') for x in array]
-array.pop(0)
-#print(array)
+def sort_col(i):
+    return i[n]
 
-k = len(array)
+student_list.sort(key=sort_col)
 
-test_lines = []
-j = 0
+print(student_list)
 
-for i in array:
+s = input('По увеличению возраста всех студентов на 1 - (3):')
+s = int(s)-1
 
-    list_element = str(i)
-    print(list_element)
-    new_element = list_element.split(';')
-    print(new_element)
-    test_lines.append(new_element)
-  
+myfile = open('students.csv', 'w', encoding='utf-16')
+for student_row in student_list:
     
-print(test_lines)
-#lines1 = [['петя',10,130,35], ['вася',11,135,39],['женя',9,140,33],['дима',10,128,30]]
-#print(lines1)
-
-
-#n = input('Сортировать по номеру группы (1), фамилии (2), возрасту (3): ')
-#n = int(n)-1
-
-#def sort_col(i):
- #   return i[n]
-    
-
-#array.sort(key=sort_col)
-
-#for i in array:
- #   print("%7s %3d %4d %3d" % (i[0],i[1],i[2],i[3]))
-#print(lines)
-
-
-    
-    
+    student_row[s] = str(int(student_row[s]) + 1)
+    myfile.write(';'.join(student_row))
+    myfile.write('\n')
+print('You are God')
